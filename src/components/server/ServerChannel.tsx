06635e9ee4c43686e6431b1e3e71ed003a83c952
proav2323 <108835201+proav2323@users.noreply.github.com>
2023-12-16 20:23:42 -0600

    coding

--- src/components/server/ServerChannel.tsx
+++ src/components/server/ServerChannel.tsx
@@ -30,7 +30,7 @@ export default function ServerChannel({channel, server, role} : {channel: Channe
     }
 
   return (
-     <button onClick={onClick} className={cn("group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1", params.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700")}>
+     <button onClick={onClick} className={cn("group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1", params!.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700")}>
           <Icon className="h-5 w-5 mr-2 flex-shrink-0 text-zinc-500 dark:text-zinc-400" />
           <p className={cn("line-clamp-1 font-semibold text-xs text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition", params?.channelId === channel.id && "text-primary dark:text-zinc-200 dark:group-hover")}>{channel.name}</p>
           {channel.name !== "general" && role !== membolRole.GUEST && (
